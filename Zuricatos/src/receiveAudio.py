2f3b8b81a55e1a65a0df358c3385064385effab5
Adolfo Hernandez <adolhdzf@gmail.com>
2024-10-25 16:56:52 -0600

    feat: serverless project

---
+++ Zuricatos/src/receiveAudio.py
@@ -0,0 +1,11 @@
+import json
+
+def receiveAudio(event, context):
+    print(event)
+    body = {
+        "message": "Go Serverless v4.0! Your function executed successfully!",
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
