513bc3a376c63e07e6305fd50b4f46bc5d278284
LuisSalazar02 <luissalazar2002@hotmail.com>
2024-10-26 00:17:28 -0600

    Feat: queryDataBase

---
+++ zuricatosJS/src/database/dbPool.js
@@ -0,0 +1,14 @@
+// db.js
+const { Pool } = require("pg");
+
+// Create a pool instance with database configuration
+const dbPool = new Pool({
+  user: "nano_store_bd",
+  host: "dpg-cse1m60gph6c73bo46og-a.oregon-postgres.render.com",
+  database: "nano_store_bd",
+  password: "DopMfU8fNwnMKdH8LGSWXd6lmMjt4fzt",
+  port: 5432,
+});
+
+// Export the pool to be used in other files
+module.exports = dbPool;
--- zuricatosJS/src/database/query.js
+++ zuricatosJS/src/database/query.js
@@ -1,8 +1,30 @@
+const dbPool = require("./dbPool");
+
 module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
-  };
+  const { query, params = [] } = event; // Destructure query and params from event input
+  let client;
+
+  try {
+    client = await dbPool.connect(); // Acquire a client from the pool
+
+    const result = await client.query(query, params);
+    const isSelectQuery = query.trim().toLowerCase().startsWith("select");
+
+    return {
+      statusCode: 200,
+      body: isSelectQuery
+        ? JSON.stringify(result.rows)
+        : "Query executed successfully",
+    };
+  } catch (error) {
+    console.error("Database query failed", error); // Log the error details for debugging
+    return {
+      statusCode: 400,
+      body: `Query execution failed: ${error.message}`,
+    };
+  } finally {
+    if (client) {
+      client.release(); // Always release the client
+    }
+  }
 };
